60b1475a4d27e74c2ff9d8aa76118b53286756b2
RRMIZ <ravireshmi2@gmail.com>
2020-12-15 20:20:07 +1300

    changed type of parcel, created availableparcel size

---
+++ ShippingPostCalculator/AvailableParcelSizes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShippingPostCalculator
+{
+    class AvailableParcelSizes
+    {
+        readonly Parcel SmallParcel = new Parcel("Small", 3, 0, 10);
+        readonly Parcel MediumParcel = new Parcel("Medium", 8,11, 50);
+        readonly Parcel LargeParcel = new Parcel("Large", 15, 51, 99);
+        readonly Parcel XLParcel = new Parcel("XL", 25, 100);
+    }
+}
