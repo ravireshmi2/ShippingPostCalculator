cad1bb566e5f32011a7d898835c47a018fcdfa2f
RRMIZ <ravireshmi2@gmail.com>
2020-12-15 20:45:15 +1300

    added calculation logic

--- ShippingPostCalculator/ParcelCalculator.cs
+++ ShippingPostCalculator/ParcelCalculator.cs
@@ -1,6 +1,22 @@
 ﻿namespace ShippingPostCalculator
 {
-    public class ParcelCalculator
+    public class ParcelCalculator : BaseCalculator
     {
+
+        public Parcel CalculateCheapestParcelType(int size)
+        {
+            if (size > AvailableParcelSizes.SmallParcel.MinSize && size < AvailableParcelSizes.SmallParcel.MaxSize)
+            {
+                return AvailableParcelSizes.SmallParcel;
+            }
+            else if (size > AvailableParcelSizes.MediumParcel.MinSize && size < AvailableParcelSizes.MediumParcel.MaxSize)
+            {
+                return AvailableParcelSizes.MediumParcel;
+            }
+            else 
+            {
+                return AvailableParcelSizes.XLParcel;
+            }
+        }
     }
 }
