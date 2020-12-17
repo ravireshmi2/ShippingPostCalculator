0fef50f0fb303a4bfba90731eee80c370f435175
RRMIZ <ravireshmi2@gmail.com>
2020-12-17 20:06:28 +1300

    added tests

---
+++ ShippingPostCalculatorTest/ParcelCalculatorTests.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShippingPostCalculator;
+
+namespace ShippingPostCalculatorTest
+{
+    [TestClass]
+    public class ParcelCalculatorTests
+    {
+        [TestMethod]
+        public void WhenSizeIsAnInteger_ExpectSmallParcelSizeToBeReturned()
+        {
+            //Arrange
+            ParcelCalculator calc = new ParcelCalculator();
+            int input = 0;
+
+            //Act
+            var result = calc.CalculateCheapestParcelType(input);
+
+            Assert.IsTrue(result.Name.Equals(AvailableParcelSizes.SmallParcel.Name));
+            Assert.IsTrue(result.Cost.Equals(AvailableParcelSizes.SmallParcel.Cost));
+
+        }
+        [TestMethod]
+        public void WhenSizeIsInMinBoundarySizeLike50_ExpectLargeParcelToBeReturned()
+        {
+            //Arrange
+            ParcelCalculator calc = new ParcelCalculator();
+            int input = 50;
+
+            //Act
+            var result = calc.CalculateCheapestParcelType(input);
+
+            Assert.IsTrue(result.Name.Equals(AvailableParcelSizes.LargeParcel.Name));
+            Assert.IsTrue(result.Cost.Equals(AvailableParcelSizes.LargeParcel.Cost));
+
+        }
+        [TestMethod]
+        public void WhenSizeIsInMaxBoundarySizeLike49_ExpectMediumParcelToBeReturned()
+        {
+            //Arrange
+            ParcelCalculator calc = new ParcelCalculator();
+            int input = 49;
+
+            //Act
+            var result = calc.CalculateCheapestParcelType(input);
+
+            Assert.IsTrue(result.Name.Equals(AvailableParcelSizes.MediumParcel.Name));
+            Assert.IsTrue(result.Cost.Equals(AvailableParcelSizes.MediumParcel.Cost));
+
+        }
+    }
+}
